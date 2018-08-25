959bf020782f90df0f1e04b091f09f73f762961e
Guilherme <gui.andra142@gmail.com>
2018-08-25 16:19:01 -0300

    color added, minus

--- public/javascripts/js.js
+++ public/javascripts/js.js
@@ -5,9 +5,29 @@ $('.click-item').click(function (e) {
 
     axios.post('file', { file: file }).then((res) => {
 
-        console.log(res.data)
+        let contentFile = res.data.split('\n')
+
+        let text = ''
+
+        $.each(contentFile, (i, row) => {
+            
+            if(row.slice(0,1) == '+'){
+
+                text += '<span class="added">' + row + '</span>' + "\n"
+
+            }else if(row.slice(0,1) == '-'){
+
+                text += '<span class="minus">' + row + '</span>' + "\n"
+
+            }else{
+
+                text += '<span>' + row + '</span>' + "\n"
+
+            }
+            
+        })
         
-        $('#content-file').text(res.data)
+        $('#content-file').html(text)
 
     })
 })
