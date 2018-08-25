1400f62f1cc1c66ed271116a545018b754b7715a
Guilherme <gui.andra142@gmail.com>
2018-08-25 18:09:56 -0300

    changes layout

--- public/javascripts/js.js
+++ public/javascripts/js.js
@@ -3,12 +3,18 @@ $(document).ready(function() {
 
     axios.get('file').then((res) => {
 
-        var diff2htmlUi = new Diff2HtmlUI({diff: res.data});
+        if(res.data.length > 0){
+            
+            $('.nothing').removeClass('hidden')
+
+            var diff2htmlUi = new Diff2HtmlUI({diff: res.data});
         
-        diff2htmlUi.draw('#content-file', {inputFormat: 'diff', outputFormat: 'side-by-side', showFiles: true, matching: 'lines', synchronisedScroll: true});
-        
-        diff2htmlUi.fileListCloseable('#content-file', true);
-        // diff2htmlUi.highlightCode('#content-file');
+            diff2htmlUi.draw('#content-file', {inputFormat: 'diff', outputFormat: 'side-by-side', showFiles: true, matching: 'lines', synchronisedScroll: true});
+            
+            diff2htmlUi.fileListCloseable('#content-file', true);
+        }else{
+            $('.nothing').addClass('hidden')
+        }
     
     })
 
