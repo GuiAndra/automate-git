b31c49c6a39e4252b8a1d04afd34d8b27f30acc4
Guilherme <gui.andra142@gmail.com>
2018-08-25 17:05:14 -0300

    add diff2html

--- public/javascripts/js.js
+++ public/javascripts/js.js
@@ -5,29 +5,12 @@ $('.click-item').click(function (e) {
 
     axios.post('file', { file: file }).then((res) => {
 
-        let contentFile = res.data.split('\n')
-
-        let text = ''
-
-        $.each(contentFile, (i, row) => {
-            
-            if(row.slice(0,1) == '+'){
-
-                text += '<span class="added">' + row + '</span>' + "\n"
-
-            }else if(row.slice(0,1) == '-'){
-
-                text += '<span class="minus">' + row + '</span>' + "\n"
-
-            }else{
-
-                text += '<span>' + row + '</span>' + "\n"
-
-            }
-            
-        })
+        var diffHtml = Diff2Html.getPrettyHtml(
+            res.data,
+            {inputFormat: 'diff', showFiles: true, matching: 'lines', outputFormat: 'side-by-side'}
+          );
         
-        $('#content-file').html(text)
+        document.getElementById("content-file").innerHTML = diffHtml;
 
     })
 })
