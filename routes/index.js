eea9bc36c531b356fafb3c644bef171ff1cc8592
Guilherme <gui.andra142@gmail.com>
2018-08-25 18:03:19 -0300

    changes

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,14 @@ const express = require('express');
 const router = express.Router();
 const git = require('simple-git')(require('path').resolve('./'))
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
 
     git.status((err, status) => {
-        res.render('index', { title: 'Automate Git', status: status });
+        res.render('index', { title: 'Gui Git', status: status });
     })
 });
 
 router.get('/file', function(req, res, next) {
-
-    // let file = req.body.file
 
     git.diff((err, diff) => {
         res.send(diff);
