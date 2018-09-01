3f40296d7075d371011545eb5a8808e363d8d6c2
Guilherme <gui.andra142@gmail.com>
2018-09-01 18:22:03 -0300

    list repositories, dumpautoload function

--- routes/index.js
+++ routes/index.js
@@ -6,17 +6,22 @@ require('dotenv').config()
 
 router.get('/', function(req, res, next) {
 
-    let repositories = []
-
-    fs.readdir(process.env.REPOSITORIES_PATH, function(err, items) {
-     
-        repositories = items
+    fs.readdir(process.env.REPOSITORIES_PATH, (err, items) => {
         
+        let repositories = items
+
+        let excludeRrepo = process.env.EXCLUDE_REPO.split(',')
+
+        for (i = 0; i < excludeRrepo.length; i++) {
+            let index = repositories.indexOf(excludeRrepo[i])
+            repositories.splice(index, 1)
+        }
+
+        // console.log(repositories)
+
+        res.render('index', { title: 'Cplug Git', repositories: repositories });
+
     });
-
-    console.log(respositories)
-
-    res.render('index', { title: 'Cplug Git', repositories: repositories });
 
 });
 
@@ -24,9 +29,44 @@ router.post('/diff', function(req, res, next) {
 
     let repo = req.body.repo
 
-    require('simple-git')('/var/www/' + repo).diff((err, diff) => {
-        res.send(diff);
-    })
+    let excludeRrepo = process.env.EXCLUDE_REPO.split(',')
+
+    //Repositórios sem permissão
+    if(excludeRrepo.includes(repo)){
+
+        res.status(401).send('Unauthorized!');
+
+    }else{
+
+        require('simple-git')(process.env.REPOSITORIES_PATH + '/' + repo).diff((err, diff) => {
+            res.send(diff);
+        })
+
+    }
+});
+
+router.post('/dumpautoload', function(req, res, next) {
+
+    let repo = req.body.repo
+
+    let excludeRrepo = process.env.EXCLUDE_REPO.split(',')
+
+    //Repositórios sem permissão
+    if(excludeRrepo.includes(repo)){
+
+        res.status(401).send('Unauthorized!');
+
+    }else{
+
+        let exec = require('child_process').exec;
+
+        let command = 'php ' + process.env.REPOSITORIES_PATH + '/' + repo + '/artisan dumpautoload'
+
+        exec(command, function(error, stdout, stderr){
+            res.send(stdout);
+        });
+
+    }
 });
 
 module.exports = router;
